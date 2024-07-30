d3e918ff03d4b15493513b962f74cf537edbdf29
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-07-29 17:21:54 -0700

    added click events

--- game.js
+++ game.js
@@ -10,17 +10,23 @@ function nextSequence(){
     const buttonColours = ["red", "blue", "green", "yellow"];
 
     const randomChosenColour = buttonColours[randomNumber];
+    
 
     gamePattern.push(randomChosenColour)
 
+    $("#" + randomChosenColour).fadeIn(20).fadeOut(20).fadeIn(20);
 
-    return randomChosenColour;
 
+    // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // audio.play();
+
+    
 }
 
 
-const log = nextSequence();
 
-console.log (log);
+$(".btn").click(function(){
+    const userChosenColour = $(this).attr("id");
+    alert("you clicked " + userChosenColour)
+})
 
-console.log(gamePattern)
